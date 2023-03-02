f79ae5f8ee1c24209ca3e7ede51f101995103ec5
BMerchant <112279397+BMerchant123@users.noreply.github.com>
2023-03-02 16:33:09 -0600

    Uploading files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+function botChange(){
+	if (document.getElementById("not-bot").value == "I aM n0t a b*t") {
+		document.getElementById("Submit").style.visibility = "visible";
+	}
+	
+	else {
+		document.getElementById("Submit").style.visibility = "none";
+	}
+}
