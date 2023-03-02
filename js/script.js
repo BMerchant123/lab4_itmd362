33cc10de8268e56823eadf17478e1784e592c068
BMerchant <112279397+BMerchant123@users.noreply.github.com>
2023-03-02 17:11:00 -0600

    Fixing indentation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
 function botChange(){
-	if (document.getElementById("not-bot").value == "I aM n0t a b*t") {
-		document.getElementById("Submit").style.visibility = "visible";
-	}
+  if (document.getElementById("not-bot").value == "I aM n0t a b*t") {
+    document.getElementById("Submit").style.visibility = "visible";
+  }
 	
-	else {
-		document.getElementById("Submit").style.visibility = "none";
-	}
+  else {
+    document.getElementById("Submit").style.visibility = "none";
+  }
 }
