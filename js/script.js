4eb6c9016729408856d46e27f1debbd068b3e58a
BMerchant <112279397+BMerchant123@users.noreply.github.com>
2023-03-23 14:05:57 -0500

    Changing the js file to send an alert if the user doesn't fill in the captcha correctly

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-function botChange(){
-  var captcha = document.getElementById("not-bot");
-  if (captcha == "I aM n0t a b*t") {
-  document.getElementById("submit").style.visibility = "visible";
-}
-	
-  else {
-    document.getElementById("submit").style.visibility = "hidden";
+function filled(){
+  var TOS = document.forms["betaTest"]["not-bot"].value
+  if (TOS == 123987) {
+    alert("Thank you for subscribing to our notification feed. We will notify you of any updates regarding our beta test")
+  } else{
+	alert("Please verify that you are not a bot");
+    return false;
   }
 }
+
